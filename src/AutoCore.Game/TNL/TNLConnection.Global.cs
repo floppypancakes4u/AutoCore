589219cb58e4d5bbee883c6cf7bb2d277d010d50
Blumster <sarkany0@hotmail.com>
2023-05-20 11:48:34 +0200

    When entering the sector server, the global server will update the map for the character, if this is the first login with it

--- src/AutoCore.Game/TNL/TNLConnection.Global.cs
+++ src/AutoCore.Game/TNL/TNLConnection.Global.cs
@@ -3,6 +3,7 @@
 namespace AutoCore.Game.TNL;
 
 using AutoCore.Database.Char;
+using AutoCore.Game.CloneBases;
 using AutoCore.Game.Managers;
 using AutoCore.Game.Packets.Global;
 
@@ -34,6 +35,44 @@ public partial class TNLConnection
 
         if (character == null)
             return;
+
+        // New character, that never entered the world
+        if (character.LastTownId == -1)
+        {
+            var characterCloneBase = AssetManager.Instance.GetCloneBase<CloneBaseCharacter>(character.CBID);
+            if (characterCloneBase == null)
+            {
+                Disconnect("Invalid character");
+
+                return;
+            }
+
+            var configNewCharacter = AssetManager.Instance.GetConfigNewCharacterFor(characterCloneBase.CharacterSpecific.Race, characterCloneBase.CharacterSpecific.Class);
+            if (configNewCharacter == null)
+            {
+                Disconnect("Invalid character");
+
+                return;
+            }
+
+            var map = MapManager.Instance.GetMap(configNewCharacter.StartTown);
+            if (map == null)
+            {
+                Disconnect("Invalid character");
+
+                return;
+            }
+
+            character.EnterMap(map);
+
+            // HACK, the character should be loaded in the above context, so save it back.
+            // even if the character is loaded form cache/reloaded from DB later, it should have a townid now, so this code isn't triggered
+            // otherwise the context would already be detached from the DBData inside Character, which would be bad/not working
+            context.SaveChanges();
+        }
+
+        // TODO: select sector server based on registered sector servers
+        // default to localhost, for now
 
         SendGamePacket(new TransferToSectorPacket()
         {
