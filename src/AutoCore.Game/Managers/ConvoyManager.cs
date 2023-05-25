1aa6f5a83f249649db4258336ee25db96bb7f9a6
Blumster <sarkany0@hotmail.com>
2023-05-26 01:42:39 +0200

    Add skeleton of the Convoy manager

---
+++ src/AutoCore.Game/Managers/ConvoyManager.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoCore.Game.Managers;
+
+using AutoCore.Game.Entities;
+using AutoCore.Game.Packets;
+using AutoCore.Utils.Memory;
+
+public class ConvoyManager : Singleton<ConvoyManager>
+{
+    public void BroadcastPacket(Character source, BasePacket packet)
+    {
+        // TODO
+    }
+}
